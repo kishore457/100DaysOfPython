1e83d5723dfc9087e9fd596e4e9c6df270d7b51a
kishore <kishoresmashes@gmail.com>
2023-03-25 18:03:42 +0530

    files created on Mar 24 2023 Day17 - BenefitsOfOops

--- Day17_BenefitsOfOOP/Challenge_QuizGame/main.py
+++ Day17_BenefitsOfOOP/Challenge_QuizGame/main.py
@@ -1,7 +1,28 @@
+from quiz_brain import QuizBrain
 from question_model import Question
 from data import question_data
 # list of question objects
+#1 start
 question_bank = []
-for i in question_data:
-    question = Question(i["text"], i["answer"])
-    question_bank.append(question)
+for question in question_data:
+    question_text = question["question"]
+    question_answer = question["correct_answer"]
+    new_question = Question(question_text, question_answer)
+    question_bank.append(new_question)
+
+print(question_bank)
+#1 end
+# 2 start
+quiz = QuizBrain(question_bank)
+# 2 end
+#3 start
+
+while quiz.still_has_questions():
+    # 2 start
+    quiz.next_question()
+    # 2 end
+
+#3 end
+
+print("You've completed the quiz")
+print(f"Your final score was : {quiz.score} / {quiz.question_number}")
